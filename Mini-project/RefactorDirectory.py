3086a9f3b31fc911a450ab59f806694f1dc907fe
Deepak Babu <deepak.babu@sysvine.com>
2020-12-28 18:41:45 +0530

    Logs not working and refactored code

--- Mini-project/RefactorDirectory.py
+++ Mini-project/RefactorDirectory.py
@@ -1,4 +1,4 @@
-# This code works only for MAC OS X
+# This code works only for MAC OSX
 
 import os
 import traceback
@@ -6,8 +6,8 @@ import logging
 
 
 logging.basicConfig(
-     filename='log_file_name.log',
-     level=logging.INFO,
+     filename='refactor_folder_log.log',
+     level=logging.DEBUG,
      format='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
      datefmt='%H:%M:%S'
  )
@@ -24,20 +24,43 @@ dir_name_index = 1
 
 
 def check_dir_exists(new_dir):
-    return os.path.exists(new_dir)
+    """
+    Checks if the directory already exists in the specified path
+
+    :param new_dir: Directory path
+    :return: True, if directory exists
+    """
+    if os.path.exists(new_dir):
+        logger.debug("%s Directory already exists", new_dir + ":")
+        return True
+    else:
+        return False
 
 
-def append_num_to_dirname(dir_name, index):
+def append_num_to_dirname(dir_name, dir_num_suffix):
+    """
+    Appends a numberic value to the directory name if duplicate exists
+
+    :param dir_name: Gets the directory name
+    :param dir_num_suffix: Gets the numberic value to be assigned
+    :return: New directory name, next numberic value for a given directory
+    """
     while True:
-        temp_dir_name = dir_name + "(" + str(index) + ")"
+        temp_dir_name = dir_name + "(" + str(dir_num_suffix) + ")"
         if check_dir_exists(temp_dir_name):
-            index += 1
+            dir_num_suffix += 1
             continue
         else:
-            return temp_dir_name, index + 1
+            return temp_dir_name, dir_num_suffix + 1
 
 
 def move_to_config(dir_path, f):
+    """
+    Moves properties file to hidden folder config
+
+    :param dir_path: Gets the basepath of the properties file.
+    :param f: Gets the properties file
+    """
     config_dir_name = ".config"
     if not check_dir_exists(dir_path + "/" + config_dir_name):
         os.mkdir(dir_path + "/" + config_dir_name)
@@ -50,6 +73,13 @@ def move_to_config(dir_path, f):
 
 
 def move_file_collection(dir_path, sub_dir_name):
+    """
+    Moves all files except properties file into the sub-directory created using the
+    base directory suffix.
+
+    :param dir_path: Gets the path of the directory
+    :param sub_dir_name: Gets the suffix name of the directory
+    """
     list_files = os.listdir(dir_path)
     for f in list_files:
         if f == sub_dir_name or f == ".config":
@@ -57,37 +87,36 @@ def move_file_collection(dir_path, sub_dir_name):
         if f.endswith(".properties"):
             move_to_config(dir_path, f)
             continue
-        existing_file = dir_path + "/" + f
-        new_file = dir_path + "/" + sub_dir_name + "/" + f
+        existing_filepath = dir_path + "/" + f
+        new_filepath = dir_path + "/" + sub_dir_name + "/" + f
         try:
-            os.rename(existing_file, new_file)
+            os.rename(existing_filepath, new_filepath)
         except OSError:
             traceback.print_exc()
-            logger.exception(traceback.print_exc())
+            logger.exception("Failed to move files!!!")
             return False
     return True
 
 
-for root, dirs, files in os.walk(path):
-    for d in dirs:
-        for i in range(len(suffices)):
-            if (d.rfind(suffices[i])) > 0:
-                try:
-                    existing_dir_path = path + d
-                    new_dir_name = d.rstrip(suffices[i]).rstrip("_")
-                    new_dir_path = path + new_dir_name
-                    if check_dir_exists(new_dir_path):
-                        new_dir_path, dir_name_index = append_num_to_dirname(new_dir_path, dir_name_index)
-                    os.rename(existing_dir_path, new_dir_path)
-                    subdir_path = new_dir_path + "/" + suffices[i]
-                    if not check_dir_exists(subdir_path):
-                        os.mkdir(subdir_path)
-                    if move_file_collection(new_dir_path, suffices[i]):
-                        print("Moved files!!!")
-                    else:
-                        print("Failed to move files!!!")
-                    break
-                except OSError:
-                    traceback.print_exc()
-                    logger.exception(traceback.print_exc())
-dir_name_index = 1
+if __name__ == "__main__":
+    for root, dirs, files in os.walk(path):
+        for directory in dirs:
+            for index in range(len(suffices)):
+                if (directory.rfind(suffices[index])) > 0:
+                    try:
+                        existing_dir_path = path + directory
+                        new_dir_name = directory.rstrip(suffices[index]).rstrip("_")
+                        new_dir_path = path + new_dir_name
+                        if check_dir_exists(new_dir_path):
+                            new_dir_path, dir_name_index = append_num_to_dirname(new_dir_path, dir_name_index)
+                        os.rename(existing_dir_path, new_dir_path)
+                        subdir_path = new_dir_path + "/" + suffices[index]
+                        if not check_dir_exists(subdir_path):
+                            os.mkdir(subdir_path)
+                        if move_file_collection(new_dir_path, suffices[index]):
+                            print("Moved files successfully!!!")
+                        break
+                    except OSError:
+                        traceback.print_exc()
+                        logger.exception("Exception occurred:")
+    dir_name_index = 1
