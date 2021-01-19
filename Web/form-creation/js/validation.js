5ef0f780ec776d7238ce08790ea1606955b3d42a
Deepak Babu <deepak.babu@sysvine.com>
2021-01-19 15:06:36 +0530

    Display age when selecting date from datepicker

--- Web/form-creation/js/validation.js
+++ Web/form-creation/js/validation.js
@@ -11,3 +11,23 @@ function only_number(evt) {
 function currentDate() {
     document.getElementById('patient-dob').value = new Date().toISOString().substring(0, 10);
 }
+
+function calculateAge() {
+    var curDate = new Date().toISOString().substring(0,10);
+    var specifiedDate = document.getElementById('patient-dob').value
+    var formattedCurDate = new Date(curDate);
+    var formattedSpecifiedDate = new Date(specifiedDate);
+    const milliSeconds = Math.ceil(formattedCurDate - formattedSpecifiedDate)
+    var totaldays = Math.ceil(milliSeconds/(1000 * 60 * 60 * 24));
+    var year = Math.floor(totaldays/365);
+    if (year) {
+        totaldays = totaldays - (year * 365);
+    }
+    var month = Math.floor(totaldays/30);
+    if (month) {
+        totaldays = totaldays - (month * 30);
+    }
+    document.getElementById('patient-age-year').value = year;
+    document.getElementById('patient-age-month').value = month;
+    document.getElementById('patient-age-date').value = totaldays;
+}
